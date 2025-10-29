dafd40b77f3ebabd13a113f56fdfbb8f3704eb78
WakkaTrout <36018858+WakkaTrout@users.noreply.github.com>
2025-10-29 09:01:26 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+int main( void )
+{
+  // TODO: 
+  // Test 1: Random 2D Point Sets of various sizes
+  // Calculate MOBB and MaxOBB using the various algorithms.
+  // Time them and graph the results
+
+  // Test 2: Structured 2D Point Sets of various sizes
+  // Calculate MOBB and MaxOBB using the various algorithms.
+  // Time them and graph the results
+
+  // Test 3: Random 3D Point Sets of various sizes
+  // Calculate MOBB and MaxOBB using the various algorithms.
+  // Time them and graph the results
+
+  // Test 4: Structured 3D Point Sets of various sizes
+  // Calculate MOBB and MaxOBB using the various algorithms.
+  // Time them and graph the results
+  return 0;
+}
